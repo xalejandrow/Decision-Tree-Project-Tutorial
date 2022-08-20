6a886ab576f965a8359143b8153bff4406d82291
Alew <xalejandrow@gmail.com>
2022-08-20 17:52:05 -0300

    Ejercicio terminado Decision Tree con modelo

--- src/app.py
+++ src/app.py
@@ -1,4 +1,33 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import folium
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn import tree
+import pickle
 
-# your code here
+url='https://raw.githubusercontent.com/4GeeksAcademy/decision-tree-project-tutorial/main/diabetes.csv'
+df_raw=pd.read_csv(url)
+df_filter = df_raw.copy()
+df_filter = df_raw[(df_raw["BMI"] > 0 ) & (df_raw["BloodPressure"] > 0) & (df_raw["Glucose"] > 0)]
+#split
+df = df_filter.copy() 
+X = df.drop('Outcome', axis=1)
+y = df['Outcome']
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=208)
+#model
+params = {'criterion': ['gini', 'entropy'], 'max_depth': range(1,10), 'min_samples_split':range(1,10), 'min_samples_leaf': range(1,5)}
+grid = GridSearchCV(DecisionTreeClassifier(random_state=0), params, verbose=1, n_jobs=-1,cv=3)
+grid.fit(X_train, y_train)
+model_cv = grid.best_estimator_
+#save
+filename = '../models/final_model.sav'
+pickle.dump(model_cv, open(filename, 'wb')) 
